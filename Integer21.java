c5336a8cc14ccd00bcb0c2f731fe003005fa4678
strange76 <strange761@gmail.com>
2015-09-30 11:47:19 +0300

    Integer21.java
    С начала суток прошло N секунд (N — целое). Найти количество секунд, прошедших с начала последней минуты.

---
+++ Integer21.java
@@ -0,0 +1,13 @@
+
+public class Integer21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int nSec=128;
+
+int sec=nSec%60;
+
+System.out.println("— начала последней минуты прошло "+sec+ " секунд");
+	}
+
+}
