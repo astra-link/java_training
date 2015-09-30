3fe30faf1d5d056c799a8ff09ec95195350adc82
strange76 <strange761@gmail.com>
2015-09-30 11:29:51 +0300

    Integer8.java
    Дано двузначное число. Вывести число, полученное при перестановке цифр исходного числа.

---
+++ Integer8.java
@@ -0,0 +1,15 @@
+
+public class Integer8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=56, tens, unit;
+
+		tens=a/10;
+		unit=a%10;
+		
+System.out.println("Ответ: "+unit+tens);
+
+	}
+
+}
