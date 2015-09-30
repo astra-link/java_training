407a930c6c7d34cff8053675cf4472142beacc03
strange76 <strange761@gmail.com>
2015-09-30 11:04:00 +0300

    Begin35.java
    Скорость лодки в стоячей воде V км/ч, скорость течения реки U км/ч (U < V). Время движения лодки по озеру T1 ч, а по реке (против течения) — T2 ч. Определить путь S, пройденный лодкой (путь = время · скорость). Учесть, что при движении против течения скорость лодки уменьшается на величину скорости течения.

---
+++ Begin35.java
@@ -0,0 +1,17 @@
+
+public class Begin35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float v=4; //boat speed on lake km/h
+float u=3; //river speed km/h
+float t1=2; //time on lake
+double t2=1.5; //time on river
+
+double s=t2*(v-u)+t1*v;
+
+System.out.println("путь пройденный лодкой равен "+s);
+
+	}
+
+}
