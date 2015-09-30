95f67dc1a1edee003e5e6481b275db4c14b23292
strange76 <strange761@gmail.com>
2015-09-30 11:55:46 +0300

    Integer28.java
    Дни недели пронумерованы следующим образом: 1 — понедельник, 2 — вторник, ..., 6 — суббота, 7 — воскресенье. Дано целое число K, лежащее в диапазоне 1–365, и целое число N, лежащее в диапазоне 1–7. Определить номер дня недели для K-го дня года, если известно, что в этом году 1 января было днем недели с номером N.

---
+++ Integer28.java
@@ -0,0 +1,14 @@
+
+public class Integer28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int kDay=2;
+int n=7;
+
+int nom=((((kDay-1)%7)+n-1)%7)+1;
+
+System.out.println("Номер дня недели для К-го дня при начале года в N день: "+nom);
+	}
+
+}
