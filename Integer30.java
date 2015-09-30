8a354cc790371bd3e3c3f03fc486d4ea865ca883
strange76 <strange761@gmail.com>
2015-09-30 11:58:50 +0300

    Integer30.java
    Дан номер некоторого года (целое положительное число). Определить соответствующий ему номер столетия, учитывая, что, к примеру, началом 20 столетия был 1901 год.

---
+++ Integer30.java
@@ -0,0 +1,11 @@
+
+public class Integer30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int year=2015;
+int century=year/100+1;
+System.out.println("Номер столетия для "+year+" года: "+century);
+	}
+
+}
