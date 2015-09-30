7f4448d29aaf73ca9b35c39b8bf5f50b1b7f83fb
strange76 <strange761@gmail.com>
2015-09-30 11:42:45 +0300

    Integer17.java
    Дано целое число, большее 999. Используя одну операцию деления нацело и одну операцию взятия остатка от деления, найти цифру, соответствующую разряду сотен в записи этого числа.

---
+++ Integer17.java
@@ -0,0 +1,13 @@
+
+public class Integer17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=1234;
+
+int hun=a/100%10;
+
+System.out.println("цифра, соответствующая разряду сотен - "+hun);
+	}
+
+}
