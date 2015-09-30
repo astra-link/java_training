24c4fa76073a53224ad826b11d84489dbbed2679
strange76 <strange761@gmail.com>
2015-09-30 11:37:04 +0300

    Integer13.java
    Дано трехзначное число. В нем зачеркнули первую слева цифру и приписали ее справа. Вывести полученное число.

---
+++ Integer13.java
@@ -0,0 +1,14 @@
+
+public class Integer13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=753;
+
+int hun=a/100;
+int twoDigit=a%100;
+
+System.out.println("новое число "+twoDigit+hun);
+	}
+
+}
