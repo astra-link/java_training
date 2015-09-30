4c8f72ab8f0212d6666a427938d894f78a11c0be
strange76 <strange761@gmail.com>
2015-09-30 11:51:43 +0300

    Integer24.java
    Дни недели пронумерованы следующим образом: 0 — воскресенье, 1 — понедельник, 2 — вторник, ..., 6 — суббота. Дано целое число K, лежащее в диапазоне 1–365. Определить номер дня недели для K-го дня года, если известно, что в этом году 1 января было понедельником.

---
+++ Integer24.java
@@ -0,0 +1,13 @@
+
+public class Integer24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int kDay=15;
+
+int nomDay=kDay%7;
+
+System.out.println("Номер дня недели "+nomDay);
+	}
+
+}
