9b508324ed26511839326bf6d1ca43bd34103ecf
strange76 <strange761@gmail.com>
2015-09-30 11:26:49 +0300

    Integer7.java
    Дано двузначное число. Найти сумму и произведение его цифр.

---
+++ Integer7.java
@@ -0,0 +1,17 @@
+
+public class Integer7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=56, sum, prod, tens, unit;
+
+tens=a/10;
+unit=a%10;
+
+
+System.out.println("сумма цифр равна "+(unit+tens));
+System.out.println("произведение цифр равно "+unit*tens);
+
+	}
+
+}
