9fe913ed4f228ea702428951757a505632dbc86b
strange76 <strange761@gmail.com>
2015-09-30 11:35:53 +0300

    Integer12.java
    Дано трехзначное число. Вывести число, полученное при прочтении исходного числа справа налево.

---
+++ Integer12.java
@@ -0,0 +1,16 @@
+
+public class Integer12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=587;
+
+int hun=a/100;
+int tens=a%100/10;
+int unit=a%10;
+
+System.out.println("Новое число "+unit+tens+hun);
+
+	}
+
+}
