1a4b47619cb04553a6c8adb619115e51c4fcad34
strange76 <strange761@gmail.com>
2015-09-30 11:10:01 +0300

    Begin39.java
    Найти корни квадратного уравнения, заданного своими коэффициентами A, B, C (коэффициент A не равен 0), если известно, что дискриминант уравнения положителен. Вывести вначале меньший, а затем больший из найденных корней.

---
+++ Begin39.java
@@ -0,0 +1,19 @@
+
+public class Begin39 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float a=2,b=-10,c=2;
+
+float d=(b*b)-4*a*c;
+System.out.println("Дискриминант равен "+d);
+
+double x1=((-b)+Math.sqrt(d))/(2*a);
+double x2=((-b)-Math.sqrt(d))/(2*a);
+
+System.out.println("x2 = "+x2);
+System.out.println("x1 = "+x1);
+
+	}
+
+}
