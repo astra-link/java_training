c58ef010a3a4ee7d3ecf0cc57476380e24f6b20d
strange76 <strange761@gmail.com>
2015-09-30 11:52:45 +0300

    Integer25.java
    Дни недели пронумерованы следующим образом: 0 — воскресенье, 1 — понедельник, 2 — вторник, ..., 6 — суббота. Дано целое число K, лежащее в диапазоне 1–365. Определить номер дня недели для K-го дня года, если известно, что в этом году 1 января было четвергом.

---
+++ Integer25.java
@@ -0,0 +1,13 @@
+
+public class Integer25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int kDay=15;
+
+int nom=(kDay+3)%7;
+
+System.out.println("Номер дня недели "+nom);
+	}
+
+}
