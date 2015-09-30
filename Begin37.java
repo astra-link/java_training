1c4d934b500de1c341fecd695f7af9ad274bc634
strange76 <strange761@gmail.com>
2015-09-30 11:06:33 +0300

    Begin37.java
    Скорость первого автомобиля V1 км/ч, второго — V2 км/ч, расстояние между ними S км. Определить расстояние между ними через T часов, если автомобили первоначально движутся навстречу друг другу. Данное расстояние равно модулю разности начального расстояния и общего пути, проделанного автомобилями; общий путь = время · суммарная скорость.

---
+++ Begin37.java
@@ -0,0 +1,17 @@
+
+public class Begin37 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float v1=40;
+		float v2=60;
+		float s=200;
+		float t=1;
+
+		float s1=t*(v1+v2);
+		float s2=Math.abs(s-s1);
+
+		System.out.println("расстояние между приближающимися автомобилями через "+t+" час(-а, -ов) равно "+s1+" км");
+	}
+
+}
