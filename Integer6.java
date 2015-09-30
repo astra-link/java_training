fb398a1fdcae45bfc327dac1d48c7cc7d16ca13d
strange76 <strange761@gmail.com>
2015-09-30 11:25:23 +0300

    Integer6.java
    Дано двузначное число. Вывести вначале его левую цифру (десятки),
    а затем — его правую цифру (единицы). Для нахождения десятков использовать операцию деления нацело, для нахождения единиц — операцию взятия остатка от деления.

---
+++ Integer6.java
@@ -0,0 +1,13 @@
+
+public class Integer6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=56;
+int tens=x/10;
+int units=x%10;
+System.out.println("Десятков "+tens);
+System.out.println("Единиц "+units);
+	}
+
+}
