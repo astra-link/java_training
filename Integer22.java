f33becfd205832ccbf3e213b726e75fc8430bf0c
strange76 <strange761@gmail.com>
2015-09-30 11:48:07 +0300

    Integer22.java
    С начала суток прошло N секунд (N — целое). Найти количество секунд, прошедших с начала последнего часа.

---
+++ Integer22.java
@@ -0,0 +1,13 @@
+
+public class Integer22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int nSec=7250;
+
+int sec=nSec%3600;
+
+System.out.println("количество секунд, прошедших с начала последнего часа: "+sec);
+	}
+
+}
